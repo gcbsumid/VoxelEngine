4a0a75e2a2f643b446709ae377dce01a6a3fdb1e
Christian Sumido <gcbsumid@gmail.com>
2014-12-06 17:34:58 -0800

    Adding the Light Shaders

---
+++ VoxelEngine/shaders/LightPS.hlsl
@@ -0,0 +1,41 @@
+// globals
+Texture2D shaderTexture;
+SamplerState SampleType;
+
+// light buffer
+cbuffer LightBuffer {
+    float4 diffuseColor;
+    float3 lightDirection;
+    float padding;
+};
+
+// typedefs
+struct PixelInputType {
+    float4 position : SV_POSITION;
+    float2 tex : TEXCOORD0;
+    float3 normal : NORMAL;
+};
+
+float4 TexturePixelShader(PixelInputType input) : SV_TARGET
+{
+    float4 textureColor;
+    float3 lightDir;
+    float lightIntensity;
+    float4 color;
+
+
+    // Sample the pixel color from the texture using the sampler at this texture coordinate location.
+    textureColor = shaderTexture.Sample(SampleType, input.tex);
+
+    // invert the light direction for calculations
+    lightDir = -lightDirection;
+
+    // Calculate the amount of light on this pixel
+    lightIntensity = saturate(dot(input.normal, lightDir));
+
+    // determine amount of diffuse color based on the diffuse color combined with the light intensity
+    color = saturate(diffuseColor * lightIntensity);
+    color = color * textureColor;
+
+    return color;
+}
---
+++ VoxelEngine/shaders/LightVS.hlsl
@@ -0,0 +1,40 @@
+// Globals
+cbuffer MatrixBuffer {
+    matrix worldMatrix;
+    matrix viewMatrix;
+    matrix projectionMatrix;
+};
+
+// Typedefs input and outputs
+struct VertexInputType {
+    float4 position : POSITION;
+    float2 tex : TEXCOORD0;
+    float3 normal : NORMAL;
+};
+
+struct PixelInputType {
+    float4 position : SV_POSITION;
+    float2 tex : TEXCOORD0;
+    float3 normal : NORMAL;
+};
+
+// Shader 
+PixelInputType LightVertexShader(VertexInputType input) {
+    PixelInputType output;
+
+    // change position to be vector 4 
+    input.position.w = 1.0f;
+
+    // calculate position of vertex
+    output.position = mul(input.position, worldMatrix);
+    output.position = mul(output.position, viewMatrix);
+    output.position = mul(output.position, projectionMatrix);
+
+    output.tex = input.tex;
+
+    // calculate the normal
+    output.normal = mult(input.normal, (float3x3)worldMatrix);
+    output.normal = normalize(output.normal);
+
+    return output;
+}
